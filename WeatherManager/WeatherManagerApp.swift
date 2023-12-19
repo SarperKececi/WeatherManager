0f17f3ecf1121bac6133d61622b84750703e013f
SarperKececi <loops4terso@gmail.com>
2023-12-19 12:44:23 +0300

    Initial Commit

---
+++ WeatherManager/WeatherManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherManagerApp.swift
+//  WeatherManager
+//
+//  Created by Sarper Kececi on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
